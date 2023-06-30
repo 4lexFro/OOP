5c42fd54b486ef5723bff00371e3678d8369a44d
alexey_frolov <k43bfg@gmail.com>
2023-06-30 22:06:47 +0300

    Sniper

--- Game/Main.java
+++ Game/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
    Monk tuck = new Monk("Tuck");
    Magician merlin = new Magician("Merlin");
+   Sniper robin = new Sniper("Robin");
 }
 
     
---
+++ Game/Sniper.java
@@ -0,0 +1,10 @@
+package Game;
+
+public class Sniper extends Shooters {
+
+    public Sniper(String name) {
+        super(1, 100, "Sniper", "Sniper", 7, 7);
+    }
+
+    
+}
