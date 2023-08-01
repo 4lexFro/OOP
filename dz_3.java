7ed9062162644e4b3c82ae423b90df412d7fcbee
alexey_frolov <k43bfg@gmail.com>
2023-08-01 14:06:55 +0300

    dz_3

--- dz_3.java
+++ dz_3.java
@@ -1,3 +1,7 @@
+/*Дан следующий код, исправьте его там, где требуется 
+(задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)*/
+
+
 import java.io.FileNotFoundException;
 
 public class dz_3 {
